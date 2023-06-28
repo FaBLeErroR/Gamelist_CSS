5ba3d57b562bee6254324df2fcbd9735ea6d249d
fableerror <zherevcukd@gmail.com>
2023-06-28 22:07:26 +1000

    Add 2nd and 3rd tasks

--- gallery.js
+++ gallery.js
@@ -5,16 +5,14 @@ $(document).ready(function(){
 function createLightGallery() {
     $("#gallery").lightGallery();
 }
-// Создаем пустой массив, который будет заполняться ссылками на изображения
+
 let images = [];
 
-// Проходим циклом по всем изображениям от 1 до 22 и формируем ссылки
 for (let i = 1; i <= 4; i++) {
     let src = '/images/zelda' + i + '.jpg';
     images.push({ src: src, thumb: src, subHtml: ' ' });
 }
 
-// Добавляем в него все изображения 
 images.forEach(function(image) {
     let link = document.createElement('a');
     link.href = image.src;
